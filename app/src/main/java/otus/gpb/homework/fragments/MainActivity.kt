3c26238ae93ca6bf9dd2dff3fd121d960434f0da
antonkazakov <antonanton08@gmail.com>
2022-10-18 22:46:11 +0400

    ...

--- app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
+++ app/src/main/java/otus/gpb/homework/fragments/MainActivity.kt
@@ -1,12 +1,25 @@
 package otus.gpb.homework.fragments
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var buttonGoFragmentA: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonGoFragmentA = findViewById(R.id.Button1)
+        buttonGoFragmentA.setOnClickListener{
+            Toast.makeText(this, "нажатие", Toast.LENGTH_SHORT).show()
+            //supportFragmentManager.beginTransaction().replace(R.id.fragmentA, FragmentA.newInstance()).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentA, FragmentA()).commit()
+        }
     }
+
 }
